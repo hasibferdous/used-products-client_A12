f5d71942c61dc156ea0397649fee35a97fb7c2e6
hasibferdous <hasibferdous.ict@gmail.com>
2022-11-28 04:13:01 +0600

    booking modal done

--- src/Pages/Product/BookingProduct/BookingProduct.js
+++ src/Pages/Product/BookingProduct/BookingProduct.js
@@ -1,30 +1,64 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import toast from 'react-hot-toast';
+import { AuthContext } from '../../../contexts/AuthProvider';
 
-const BookingProduct = () => {
-    // const { title } = bike ;
-        // TODO: send data to the server
-        // and once data is saved then close the modal 
-        // and display success toast
-    return (
+const BookingProduct = ({ bike, setBike,  refetch }) => {
+    //  const { title } = bike ;
+    const { user } = useContext(AuthContext);
+    
+    const handleBooking = event => {
+        event.preventDefault();
+        const form = event.target;
+       
+        const name = form.name.value;
+        const email = form.email.value;
+        const phone = form.phone.value;
+        const location = form.location.value;
+  
+        const booking = {
+            name,
+            email,
+            phone,
+            location,
+            
+        }
+
+        fetch('http://localhost:5001/bookings', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.acknowledged) {
+                    // setBike(null);
+                    toast.success('Booking confirmed');
+                    refetch();
+                }
+                else{
+                    toast.error(data.message);
+                }
+            })
+        }
+
+        return (
         <>
             <input type="checkbox" id="booking-bike" className="modal-toggle" />
             <div className="modal">
                 <div className="modal-box relative">
                     <label htmlFor="booking-bike" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    {/* <h3 className="text-lg font-bold">{title}</h3> */}
-                    <form className='grid grid-cols-1 gap-3 mt-10'>
-<input name="name" type="text" 
-placeholder="Your Name" className="input w-full 
-input-bordered" />
- <input name="email" type="email" 
-placeholder="Email Address" className="input w-full 
-input-bordered" />
- <input name="phone" type="text" 
-placeholder="Phone Number" className="input w-full 
-input-bordered" />
- <br />
- <input className='btn btn-accent w-full' 
-type="submit" value="Submit" />
+                    {/* <h3 className="text-lg font-bold">{title}</h3>  */}
+                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 mt-10'>
+                        <input name="name" type="text" defaultValue={user?.displayName} disabled placeholder="Your Name" className="input w-full input-bordered" />
+                        <input name="email" type="email" defaultValue={user?.email} disabled placeholder="Email Address" className="input w-full input-bordered" />
+                        <input name="phone" type="text" placeholder="Phone Number" className="input w-full input-bordered" />
+                        <input name="location" type="text" placeholder="Meeting Location" className="input w-full input-bordered" />
+                        
+                        <br />
+                        <input className='btn btn-accent w-full' type="submit" value="Submit" />
  </form>
 
 
--- src/Pages/Product/Product/AvailableProduct/AvailableProduct.js
+++ src/Pages/Product/Product/AvailableProduct/AvailableProduct.js
@@ -1,4 +1,5 @@
 
+import { useQuery } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
 import BookingProduct from '../../BookingProduct/BookingProduct';
 import ProductCard from '../ProductCard';
@@ -6,20 +7,27 @@ import ProductCard from '../ProductCard';
 
 const AvailableProduct = () => {
 
-    const [products, setProducts] = useState([]);
+    // const [products, setProducts] = useState([]);
     const [bike, setBike] = useState(null);
-    useEffect(() => {
-    fetch('products.json')
-    .then(res => res.json())
-    .then(data => setProducts(data))
-    }, [])
+
+    const {data:products = []} = useQuery({
+        queryKey: ['products'],
+        queryFn: async() => {
+            const res = await fetch('http://localhost:5001/products')
+            const data = await res.json();
+            return data
+        }
+    })
+
+
+
     
 
 return (
 <section className='my-16'>
 
 
-<div className='grid gap-6 grid-cols-1 md:gridcols-2 lg:grid-cols-3 mt-6'>
+<div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
 {
 products.map(option => 
 <ProductCard
